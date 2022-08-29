b5e9d908611d6cdc2f8e2fd373b428c78a4a1539
RafaelAngeloPiad <watchingmaniac@gmail.com>
2022-08-30 00:10:31 +0800

    minor adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,12 @@ import "./css/Header.css";
 
 function Header(props) {
   return (
-    <header id="header">
-      <div className="intro">
-        <div className="overlay">
-          <div className="intro-text">
-            <h1>{props.data ? props.data.title : "Loading"}</h1>
-            <div>{props.data ? props.data.paragraph : "Loading"}</div>
-            <Button buttonColor="red">Get in Touch</Button>
-          </div>
+    <header id="header" class="header-container">
+      <div className="header-intro">
+        <div className="intro-text">
+          <h1>{props.data ? props.data.title : "Loading"}</h1>
+          <div>{props.data ? props.data.paragraph : "Loading"}</div>
+          <Button buttonColor="red">Get in Touch</Button>
         </div>
       </div>
     </header>
