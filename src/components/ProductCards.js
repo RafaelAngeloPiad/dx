b667509273dcefec32650c93994d8415c22fd4ba
RafaelAngeloPiad <watchingmaniac@gmail.com>
2022-08-30 13:23:22 +0800

    bootstrap adjustment

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -4,27 +4,27 @@ const ProductCards = (props) => {
   console.log(props.data);
   return (
     <div className="product-cards_container">
-      <div className="section-title">
+      <div className="c_section-title">
         <h2>Our Services</h2>
         <div>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
           dapibus leonec.
         </div>
       </div>
-      <div className="cards_container">
+      <div className="c_cards_container">
         {props.data.map((item) => (
-          <div className="card" key={item.id}>
-            <div className="card_img">
+          <div className="c_card" key={item.id}>
+            <div className="c_card_img">
               <img src={item.thumb} />
             </div>
-            <div className="card_header">
+            <div className="c_card_header">
               <h2>{item.product_name}</h2>
               <p>{item.description}</p>
-              <p className="price">
+              <p className="c_price">
                 {item.price}
                 <span>{item.currency}</span>
               </p>
-              <div className="cards_btn">Add to cart</div>
+              <div className="c_cards_btn">Add to cart</div>
             </div>
           </div>
         ))}
