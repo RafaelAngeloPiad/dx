f97c6ad073720a1361c829eb554e39225358bd46
RafaelAngeloPiad <watchingmaniac@gmail.com>
2022-08-31 14:11:10 +0800

    slider improved

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -8,7 +8,7 @@ export default function Slider(props) {
 
   // setTimeout(() => {
   //   nextSlide();
-  // }, 9000);
+  // }, 1000);
 
   const nextSlide = () => {
     if (slideIndex !== props.data.length) {
@@ -38,30 +38,43 @@ export default function Slider(props) {
             key={obj.id}
             className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
           >
-            <div>{obj.title}</div>
-            <img
-              src={process.env.PUBLIC_URL + `/images/img${index + 1}.jpg`}
-              alt="Slider"
-            />
+            <div className="slider-overlay-container">
+              <img
+                src={process.env.PUBLIC_URL + `/images/img${index + 1}.jpg`}
+                className="slider-img"
+                alt="Slider"
+              />
+              <div className="slider-desc">
+                <h2>{obj.title}</h2>
+                <div>{obj.subTitle}</div>
+              </div>
+              <img
+                src={process.env.PUBLIC_URL + `/images/swave.svg`}
+                className="slider-cover"
+                alt="slider-cover"
+              />
+            </div>
           </div>
         );
       })}
-      <button onClick={nextSlide} className={"btn-slide next"}>
-        <img src={rightArrow} alt="Slider Right" />
-      </button>
+      <div className="slider-control-panel">
+        <button onClick={nextSlide} className={"btn-slide next"}>
+          <img src={rightArrow} alt="Slider Right" />
+        </button>
 
-      <button onClick={prevSlide} className={"btn-slide prev"}>
-        <img src={leftArrow} alt="Slider Left" />
-      </button>
+        <button onClick={prevSlide} className={"btn-slide prev"}>
+          <img src={leftArrow} alt="Slider Left" />
+        </button>
 
-      <div className="container-dots">
-        {Array.from({ length: 5 }).map((item, index) => (
-          <div
-            key={index}
-            onClick={() => moveDot(index + 1)}
-            className={slideIndex === index + 1 ? "dot active" : "dot"}
-          ></div>
-        ))}
+        <div className="container-dots">
+          {Array.from({ length: 5 }).map((item, index) => (
+            <div
+              key={index}
+              onClick={() => moveDot(index + 1)}
+              className={slideIndex === index + 1 ? "dot active" : "dot"}
+            ></div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/data/slider-data.js
+++ src/components/data/slider-data.js
@@ -1,8 +1,9 @@
 export const dataSlider = [
   {
     id: 1,
-    title: "Lorem ipsum 1",
-    subTitle: "Lorem",
+    title: "Better Deals with better everything",
+    subTitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 2,
@@ -11,8 +12,9 @@ export const dataSlider = [
   },
   {
     id: 3,
-    title: "Lorem ipsum 3",
-    subTitle: "Lorem",
+    title: "Better Deals with better everything",
+    subTitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 4,
@@ -21,7 +23,8 @@ export const dataSlider = [
   },
   {
     id: 5,
-    title: "Lorem ipsum 5",
-    subTitle: "Lorem",
+    title: "Better Deals with better everything",
+    subTitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 ];
