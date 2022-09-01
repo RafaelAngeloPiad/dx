4c953ab3a4760069b8f2842c142f3f15064e70c9
RafaelAngeloPiad <watchingmaniac@gmail.com>
2022-09-01 15:06:26 +0800

    something

--- src/components/Header3.js
+++ src/components/Header3.js
@@ -1,14 +1,18 @@
 import { Button } from "./Button";
 import "./css/Header.css";
+import { BsArrowRight } from "react-icons/bs";
 
 function Header(props) {
   return (
     <header id="header" className="header3-container">
       <div className="header3-intro">
-        <div className="intro-text">
+        <div className="header3-intro-text">
           <h1>{props.data.title}</h1>
         </div>
         <div className="intro-desc">{props.data.paragraph}</div>
+        <Button buttonStyle="custom_btn--words">
+          See More <BsArrowRight />
+        </Button>
       </div>
     </header>
   );
--- src/components/data/header-data.js
+++ src/components/data/header-data.js
@@ -15,13 +15,26 @@ export const headerProduct = {
   headline: "Connect.Communicate.Collaborate.",
   title: "Dialogx Ltd - Carrier as a Service",
   paragraph: (
-    <div>
-      Dialogx CaaS is a cloud-based carrier infrastructure platform, designed to
-      enable service providers to deploy Unified Communications globally with
-      zero capital investment through a cloud-based or in data-centre solution.
-      Built on Kubernetes and driven by AWS' scalable infrastructure, DXP gives
-      Service Providers access to advanced communications services including
-      voice, messaging, chat, video, conferencing and business productivity.
-    </div>
+    <>
+      <div>
+        Dialogx CaaS is a cloud-based carrier infrastructure platform, designed
+        to enable service providers to deploy Unified Communications globally
+        with zero capital investment through a cloud-based or in data-centre
+        solution.
+      </div>
+      <div>
+        Built on Kubernetes and driven by AWS' scalable infrastructure, DXP
+        gives Service Providers access to advanced communications services
+        including voice, messaging, chat, video, conferencing and business
+        productivity.
+      </div>
+    </>
   ),
+};
+
+export const headerProduct2 = {
+  headline: "Connect.Communicate.Collaborate.",
+  title: "See how the world uses GoTo Meeting",
+  paragraph:
+    "Secure web conferencing learning and collaboration with students, for individualized lessons or small groups",
 };
